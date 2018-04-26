40f792d6aa8a1a2259c0563f295aaa586ec0ae7c
Beverly Pham <beverly.pham@gmail.com>
2018-04-26 15:35:09 -0700

    make homepage photo public-only photos

--- imagersite/imagersite/views.py
+++ imagersite/imagersite/views.py
@@ -5,8 +5,10 @@ import random
 
 # Create your views here.
 def home_view(request):
-    all_photos = Photo.objects.all()
-    # if not all_photos
-    one_photo = random.choice(all_photos)
-    image_url = one_photo.image.url
+    all_photos = Photo.objects.filter(published='PUBLIC')
+    try:
+        one_photo = random.choice(all_photos)
+        image_url = one_photo.image.url
+    except IndexError:
+        image_url = 'static/windows.jpg'
     return render(request, 'generic/home.html', {'image': image_url})
