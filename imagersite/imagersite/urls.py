4187479550b19e090ef4c2ffe7a3a5d8b5db89bf
Beverly Pham <beverly.pham@gmail.com>
2018-04-27 09:20:14 -0700

    change static serve setting to only debug mode

--- imagersite/imagersite/urls.py
+++ imagersite/imagersite/urls.py
@@ -25,7 +25,8 @@ urlpatterns = [
     path('accounts/', include('registration.backends.hmac.urls')),
     path('profile/', include('imager_profile.urls')),
     path('images/', include('imager_images.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
-# if settings.DEBUG is True:
-    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
