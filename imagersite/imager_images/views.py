0b3063a87b2797349c011b856ab3073bb8563b3f
Beverly Pham <beverly.pham@gmail.com>
2018-05-09 14:33:29 -0700

    add docstrings

--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -13,11 +13,13 @@ class LibraryView(ListView):
     context_object_name = 'library'
 
     def get(self, *args, **kwargs):
+        """Get."""
         if not self.request.user.is_authenticated:
             return redirect('home')
         return super().get(*args, **kwargs)
 
     def get_queryset(self):
+        """Get queryset."""
         username = self.request.user.get_username()
         total_albums = Album.objects.filter(user__username=username)
         total_photos = Photo.objects.filter(user__username=username)
@@ -25,6 +27,7 @@ class LibraryView(ListView):
         return [total_albums, total_photos]
 
     def get_context_data(self, **kwargs):
+        """Get context data."""
         context = super().get_context_data(**kwargs)
         context['albums'] = context['library'][0]
         context['photos'] = context['library'][1]
@@ -41,14 +44,17 @@ class AlbumView(ListView):
     context_object_name = 'albums'
 
     def get(self, *args, **kwargs):
+        """Get."""
         if not self.request.user.is_authenticated:
             return redirect('home')
         return super().get(*args, **kwargs)
 
     def get_queryset(self):
+        """Get queryset."""
         return Album.objects.filter(published='PUBLIC')
 
     def get_context_data(self, **kwargs):
+        """Get context."""
         context = super().get_context_data(**kwargs)
         return context
 
@@ -61,14 +67,17 @@ class PhotoView(ListView):
     context_object_name = 'photos'
 
     def get(self, *args, **kwargs):
+        """Get."""
         if not self.request.user.is_authenticated:
             return redirect('home')
         return super().get(*args, **kwargs)
 
     def get_queryset(self):
+        """Get queryset."""
         return Photo.objects.filter(published='PUBLIC')
 
     def get_context_data(self, **kwargs):
+        """Get context."""
         context = super().get_context_data(**kwargs)
         return context
 
@@ -81,15 +90,18 @@ class AlbumDetailView(ListView):
     context_object_name = 'album'
 
     def get(self, *args, **kwargs):
+        """Get."""
         if not self.request.user.is_authenticated:
             return redirect('home')
         return super().get(*args, **kwargs)
 
     def get_queryset(self):
+        """Get queryset."""
         album = Album.objects.filter(id=self.kwargs['id']).first()
         return album.photos.filter(published='PUBLIC')
 
     def get_context_data(self, **kwargs):
+        """Get context."""
         context = super().get_context_data(**kwargs)
         return context
 
@@ -102,14 +114,17 @@ class PhotoDetailView(DetailView):
     context_object_name = 'photo'
 
     def get(self, *args, **kwargs):
+        """Get."""
         if not self.request.user.is_authenticated:
             return redirect('home')
         return super().get(*args, **kwargs)
 
     def get_object(self):
+        """Get object."""
         return Photo.objects.filter(id=self.kwargs['id']).first()
 
     def get_context_data(self, **kwargs):
+        """Get context."""
         context = super().get_context_data(**kwargs)
         return context
 
@@ -123,16 +138,19 @@ class AddPhotoView(CreateView):
     success_url = reverse_lazy('library')
 
     def get(self, *args, **kwargs):
+        """Get."""
         if not self.request.user.is_authenticated:
             return redirect('home')
         return super().get(*args, **kwargs)
 
     def post(self, *args, **kwargs):
+        """Post."""
         if not self.request.user.is_authenticated:
             return redirect('home')
         return super().post(*args, **kwargs)
 
     def form_valid(self, form):
+        """Validate form."""
         form.instance.user = self.request.user
         return super().form_valid(form)
 
@@ -146,20 +164,24 @@ class AddAlbumView(CreateView):
     success_url = reverse_lazy('library')
 
     def get(self, *args, **kwargs):
+        """Get."""
         if not self.request.user.is_authenticated:
             return redirect('home')
         return super().get(*args, **kwargs)
 
     def post(self, *args, **kwargs):
+        """Post."""
         if not self.request.user.is_authenticated:
             return redirect('home')
         return super().post(*args, **kwargs)
 
     def get_form_kwargs(self):
+        """Get form kwargs."""
         kwargs = super().get_form_kwargs()
         kwargs.update({'username': self.request.user.username})
         return kwargs
 
     def form_valid(self, form):
+        """Validate form."""
         form.instance.user = self.request.user
         return super().form_valid(form)
--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -12,11 +12,13 @@ class ProfileView(ListView):
     context_object_name = 'one_profile'
 
     def get(self, *args, **kwargs):
+        """Get."""
         if not self.request.user.is_authenticated:
             return redirect('home')
         return super().get(*args, **kwargs)
 
     def get_queryset(self):
+        """Get queryset."""
         owner = False
         if 'username' not in self.kwargs:
             username = self.request.user.get_username()
@@ -34,6 +36,7 @@ class ProfileView(ListView):
         return [profile, albums, photos]
 
     def get_context_data(self, **kwargs):
+        """Get context data."""
         context = super().get_context_data(**kwargs)
         context['profile'] = context['one_profile'][0]
         context['albums'] = context['one_profile'][1]
